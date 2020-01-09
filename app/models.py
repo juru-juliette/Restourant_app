e946bd7c4b4873b7ee0a848af76773c0075dec32
juru <kangabejuliette@gmail.com>
2020-01-09 17:51:48 +0200

    configuration

---
+++ app/models.py
@@ -0,0 +1,12 @@
+class Resaurant:
+    '''
+    Resaurant class to define Resaurant Objects
+    '''
+
+    def __init__(self,id,name,location,details,daily_menu):
+        self.id =id
+        self.name = name
+        self.location = location
+        self.poster = "https://image.tmdb.org/t/p/w500/" + poster
+        self.details = details
+        self.daily_menu = daily_menu
